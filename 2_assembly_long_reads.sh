a2a39377b4cc8dd67ca21c2dd4b6ebb949884627
Marcus V. C. Viana <131043224+canarioviana@users.noreply.github.com>
2026-02-17 16:16:20 +0100

    Add files via upload

--- 2_assembly_long_reads.sh
+++ 2_assembly_long_reads.sh
@@ -1,44 +1,41 @@
 #!/bin/bash
-# Bash script for bacterial genome assembly from short-read sequencing data
+# Bash script for bacterial genome assembly from Oxford NanoPore sequencing data
 #
 # Author: Marcus Vinicius Canário Viana
-# Date: 16/02/2025
+# Date: 17/02/2025
 # Repository: https://github.com/canarioviana/workshop_amsterdam_umc
-#
+
 
 ############################################################
-## SUMMARY
+## SUMMARY OF END-TO-END GENOME ASSEMBLY WORKFLOW FROM LONG-READS
 ############################################################
 
 ## 0) Working directory
 ## 1) Sequencing reads directory and files
-    # Download reads from NCBI SRA (sra-tools)
-    # Check local reads files
+    # Reads stored as local files
 ## 2) Raw reads quality assessment
-    # FastQC
-    # MultiQC
-## 3) Raw reads trimming, estimation of genome size and downsampling 
-    # Fastp
-    # Estimation of genome size (KMC and GenomeScope) and downsampling (Rasusa)
+    # NanoPlot
+## 3) Raw reads trimming
+    # Chopper
 ## 4) Trimmed reads quality assessment
-    # FastQC
-    # MultiQC
+    ## NanoPlot
 ## 5) De novo assembly
-    # Unicycler
+    # Flye
 ## 6) Organization of de novo assembly files
 ## 7) Assembly quality assessment
     # CheckM2
     # GUNC
     # QUAST
-    # Barrnap
     # Calculation of vertical sequencing coverage
 ## 8) Taxonomic assignment
     # GTDB-Tk
     # TYGS
     # JspeciesWS
 ## 9) Plasmid identification
-    # MOB-suite 
+    # MOB-suite
 ## 10) Assignment of contigs to molecules
+    # MOB-suite and an inhouse script
+
 
 ############################################################
 ## 0) Working directory
@@ -48,10 +45,11 @@
 cd /mnt/4tb_1/workshop_umc
 
 # Create assembly directory
-mkdir -p short_reads/groupname
+mkdir -p long_reads/<groupname>
 
 # Go to assembly directory
-cd short_reads/groupname
+cd long_reads/<groupname>
+
 
 ############################################################
 ## 1) Sequencing reads directory and files
@@ -61,16 +59,19 @@ cd short_reads/groupname
 ## Reads from NCBI SRA database
 
 # Sequencing data to download
-# Leptospira interrogans serovar Copenhageni strain M26
-# BioProject: https://www.ncbi.nlm.nih.gov/bioproject/PRJNA478606
-# BioSample: https://www.ncbi.nlm.nih.gov/biosample/SAMN09521107
-# SRA Experiment: https://www.ncbi.nlm.nih.gov/sra/SRX4377423
-# SRA Run: SRR7506941
+# Leptospira interrogans strain N116
+# BioProject: https://www.ncbi.nlm.nih.gov/bioproject/PRJNA828002
+# BioSample: https://www.ncbi.nlm.nih.gov/biosample/SAMN27628310
+# Genome: https://www.ncbi.nlm.nih.gov/datasets/genome/GCF_023515975.1/
+# SRA: https://www.ncbi.nlm.nih.gov/sra/SRX27382703
+# NanoPore flow cell: R10.3
+# Basecalling model: Super accuracy model (SUP, performed with Guppy (v5.0.11+2b6dbff))
+# Raw reads accession: SRR32032837
 
-# Declare SRA Run accession
-accession=SRR7506941
+# Declare accession id
+accession=SRR32032837
 # Declare sample name
-sample=M26
+sample=N116
 
 # Create output directory
 mkdir 1_reads
@@ -100,141 +101,146 @@ rm -r "${accession}"
 pigz -p $(nproc --ignore=1) "${accession}"*.fastq
 
 # Rename files
-mv "${accession}_1.fastq.gz" "${sample}_1.fq.gz"
-mv "${accession}_2.fastq.gz" "${sample}_2.fq.gz"
+mv "${accession}.fastq.gz" "${sample}.fq.gz"
 
 # Back to assembly directory
 cd ..
+
 
 ############################################################
 ## 2) Raw reads quality assessment
 ############################################################
 
 ############################################################
-## FastQC
-# Create an output directory
-mkdir 2_fastqc
-
-# Activate Conda environment
-conda activate fastqc
-
-# Run FastQC
-fastqc -t $(nproc --ignore=1) 1_reads/*.fq.gz -o 2_fastqc
-
-# Deactivate Conda environment
-conda deactivate
-
-# Compress the output directory
-zip -r 2_fastqc.zip 2_fastqc
-
-############################################################
-## FastQC -> MultiQC
-
-# Activate Conda environment
-conda activate multiqc
-
-# Run MultiQC
-multiqc 2_fastqc/*_fastqc.zip -o 2_fastqc_multiqc
-
-# Deactivate Conda environment
-conda deactivate
-
-# Compress the output directory
-zip -r 2_fastqc_multiqc.zip 2_fastqc_multiqc
-
-# Delete the output directory
-rm -r 2_fastqc 2_fastqc_multiqc
-
-
-############################################################
-## 3) Raw reads trimming 
-############################################################
-
-############################################################
-## Fastp
+## NanoPlot
 
 # Create an output directory
-mkdir 3_fastp
+mkdir 2_nanoplot
 
 # Activate Conda environment
-conda activate fastp
+conda activate nanoplot
 
 # Loop through a list of files
-for r1 in 1_reads/*_1.fq.gz; do
-    # Extract r2 path
-    r2="${r1/_1.fq.gz/_2.fq.gz}"
-    # Extract r1 file name
-    r1filename=${r1##*/}
+for reads in 1_reads/*.fq.gz; do
+    # Extract reads file name
+    readsfilename=${reads##*/}
     # Extract sample name
-    sample=${r1filename%%_*}
+    sample=${readsfilename%%_*} # In case the file name is samplename_*.fq.gz
+    sample=${sample%%.*} # In case the file name is samplename.fq.gz
 
-    # Run Fastp
-    fastp \
-        --thread $(nproc --ignore=1) \
-        --detect_adapter_for_pe \
-        --trim_poly_g \
-        --cut_front \
-        --cut_right \
-        --cut_window_size 4 \
-        --cut_mean_quality 20 \
-        --length_required 50 \
-        --overrepresentation_analysis \
-        --in1 "$r1" \
-        --in2 "$r2" \
-        --out1 "3_fastp/${sample}_trimmed_1.fq.gz" \
-        --out2 "3_fastp/${sample}_trimmed_2.fq.gz" \
-        --html "3_fastp/${sample}_trimmed_fastp.html" \
-        --json "3_fastp/${sample}_trimmed_fastp.json"
+    # Verify if the input files are empty
+    if [ ! -s "$reads" ]; then
+        echo "Warning: The input files of sample ${sample} are empty. Skiping..."
+        echo -e "${sample}" >> 2_nanoplot_skiped_samples.tsv
+        # Skip to the next iteration
+        continue
+    fi
+
+    echo "NanoPlot is processing sample: ${sample}"
+    mkdir "2_nanoplot/${sample}"
+    NanoPlot -t $(nproc --ignore=1) --fastq ${reads} -p "${sample}_" -o "2_nanoplot/${sample}"
+done
+
+# Deactivate Conda environment
+conda deactivate
+
+# Compress the output directory
+zip -r 2_nanoplot.zip 2_nanoplot
+
+# Delete the output directory
+rm -r 2_nanoplot
+
+
+############################################################
+## 3) Raw reads trimming
+############################################################
+
+############################################################
+## Fastplong
+
+# Create an output directory
+mkdir 3_fastplong
+
+# Activate Conda environment
+conda activate fastplong
+# Loop through a list of files
+for reads in 1_reads/*.fq.gz; do
+
+    # Extract reads file name
+    readsfilename=${reads##*/}
+    # Extract sample name
+    sample=${readsfilename%%_*} # In case the file name is samplename_*.fq.gz
+    sample=${sample%%.*} # In case the file name is samplename.fq.gz
+
+    # Inform the current sample being processed
+    echo "Processing sample: ${sample}"
+    echo "Reads file ${reads}"
+
+    # Run Fastplong
+    fastplong \
+    --thread $(nproc --ignore=1) \
+    --disable_quality_filtering \
+    --length_required 500 \
+    -i "$reads" \
+    -o 3_fastplong/"${sample}_trimmed.fq.gz" \
+    --html 3_fastplong/"$sample"_fastp.html \
+    --json 3_fastplong/"$sample"_fastp.json
 done
 
 # Deactivate Conda environment
 conda deactivate
 
 # Compress report files
-zip -r 3_fastp.zip 3_fastp/*.json 3_fastp/*.html
-
+# zip -r 3_fastplong.zip 3_fastplong/*.json 3_fastplong/*.html
 # Delete report files
-rm 3_fastp/*.json 3_fastp/*.html
+# rm 3_fastplong/*.json 3_fastplong/*.html
+
 
 ############################################################
 ## 4) Trimmed reads quality assessment
 ############################################################
 
 ############################################################
-## FastQC
+## NanoPlot
 
 # Create an output directory
-mkdir 4_fastqc
+mkdir 4_nanoplot
 
 # Activate Conda environment
-conda activate fastqc
+conda activate nanoplot
 
-# Run FastQC
-fastqc -t $(nproc --ignore=1) 3_fastp/*.fq.gz -o 4_fastqc
+# Declare input directory
+input_directory="3_fastplong"
+# Loop through a list of files
+for reads in "${input_directory}"/*.fq.gz; do
+    # Extract reads file name
+    readsfilename=${reads##*/}
+    # Extract sample name
+    sample=${readsfilename%%_*} # In case the file name is samplename_*.fq.gz
+    sample=${sample%%.*} # In case the file name is samplename.fq.gz
+
+    # Verify if the input files are empty
+    if [ ! -s "$reads" ]; then
+        echo "Warning: The input files of sample ${sample} are empty. Skiping..."
+        echo -e "${sample}" >> 4_nanoplot_skiped_samples.tsv
+        # Skip to the next iteration
+        continue
+    fi
+
+    echo "NanoPlot is processing sample: ${sample}"
+    echo "A warning is expected if the Conda environment was installed by the root."
+    mkdir "4_nanoplot/${sample}"
+    NanoPlot -t $(nproc --ignore=1) --fastq ${reads} -p "${sample}_" -o "4_nanoplot/${sample}"
+done
 
 # Deactivate Conda environment
 conda deactivate
 
 # Compress the output directory
-zip -r 4_fastqc.zip 4_fastqc
-
-############################################################
-## Fastp -> FastQC -> MultiQC
-
-# Activate Conda environment
-conda activate multiqc
-
-# Run MultiQC
-multiqc 4_fastqc/*_fastqc.zip -o 4_fastqc_multiqc
-
-# Deactivate Conda environment
-conda deactivate
-
-# Compress the output directory
-zip -r 4_fastqc_multiqc.zip 4_fastqc_multiqc
+zip -r 4_nanoplot.zip 4_nanoplot
 
 # Delete the output directory
-rm -r 4_fastqc 4_fastqc_multiqc 
+rm -r 4_nanoplot
 
 
 ############################################################
@@ -242,38 +248,74 @@ rm -r 4_fastqc 4_fastqc_multiqc
 ############################################################
 
 ############################################################
-## Unicycler (~13 minutes)
+## Flye (~37 minutes)
 
 # Create an output directory
-mkdir 5_unicycler
+mkdir 5_flye
 
 # Activate Conda environment
-conda activate unicycler
+conda activate flye
 
+# Set library type
+# valid_options: --pacbio-raw | --pacbio-corr | --pacbio-hifi | --nano-raw | --nano-corr | --nano-hq
+seq_type="--nano-hq"
+
+# Declare input directory
+input_directory="3_fastplong"
 # Loop through a list of files
-for r1 in 3_fastp/*_1.fq.gz; do
-    # Extract r2 path
-    r2=${r1/_1.fq.gz/_2.fq.gz}
-    # Extract r1 file name
-    filename=${r1##*/}
+for reads in "${input_directory}"/*.fq.gz; do
+    # Extract reads file name
+    readsfilename=${reads##*/}
     # Extract sample name
-    sample=${filename%%_*}
-
-    # Run Unicycler
-    unicycler \
-    -t $(nproc --ignore=1) \
-    --spades_options "--cov-cutoff auto" \
-    --min_fasta_length 200 \
-    -1 "${r1}" \
-    -2 "${r2}" \
-    -o "5_unicycler/${sample}_unicycler"
+    sample=${readsfilename%%_*} # In case the file name is samplename_*.fq.gz
+    sample=${sample%%.*} # In case the file name is samplename.fq.gz
+    # Run Flye
+    echo "Assembling the genome of sample: $sample"
+    flye -t $(nproc --ignore=1) \
+    "${seq_type}" \
+    "$reads" \
+    -o 5_flye/"$sample"_flye
 done
 
 # Deactivate Conda environment
 conda deactivate
 
-# Compress the output directory
-zip -r 5_unicycler.zip 5_unicycler
+# Compress output files
+zip -r 5_flye.zip 5_flye
+
+############################################################
+## Flye -> Medaka
+
+# Create an output directory
+mkdir 5_flye_medaka
+
+# Activate Conda environment
+conda activate medaka
+
+# Avoid using the GPU
+CUDA_VISIBLE_DEVICES="" 
+
+# Declare inference model according to your sequencing library
+# List the available inference models using the command: medaka tools list_models
+# Current sample has NanoPore flow cell R10.3 and basecalling model SUP
+model=r103_sup_g507
+
+# Loop through a list of files
+for reads in 3_fastplong/*.fq.gz; do
+    # Extract reads file name
+    readsfilename=${reads##*/}
+    # Extract sample name
+    sample=${readsfilename%%_*} # In case the file name is samplename_*.fq.gz
+    sample=${sample%%.*} # In case the file name is samplename.fq.gz
+
+    # Run medaka
+    medaka_consensus \
+    -i ${reads} \
+    -d 5_flye/"${sample}"_flye/assembly.fasta \
+    -o 5_flye_medaka/"${sample}"_medaka \
+    -t $(nproc --ignore=1) \
+    -m "${model}"
+done
 
 
 ############################################################
@@ -282,19 +324,20 @@ zip -r 5_unicycler.zip 5_unicycler
 
 ############################################################
 ## Create assemblies directory
+
 mkdir 6_assemblies
 
 ############################################################
-## Organize assemblies from Unicycler
+## Organizing assemblies from Flye -> Medaka
 
 # Loop through a list of directories
-for dir in 5_unicycler/*/; do
+for dir in 5_flye_medaka/*/; do
     # Extract directory name
     dirname=${dir#*/}
     # Extract sample name
-    sample=${dirname%%_unicycler*}
+    sample=${dirname%%_medaka*}
     # Copy and rename the assembly file
-    cp "${dir}assembly.fasta" "6_assemblies/${sample}.fasta"
+    cp "${dir}consensus.fasta" "6_assemblies/${sample}.fasta"
 done
 
 ############################################################
@@ -303,8 +346,9 @@ done
 zip -r 6_assemblies.zip 6_assemblies
 
 ############################################################
-## Delete the assembler output directory
-rm -r 5_unicycler
+## Delete the assemblers output directories
+
+rm -r 5_flye
 
 
 ############################################################
@@ -312,7 +356,7 @@ rm -r 5_unicycler
 ############################################################
 
 ############################################################
-## CheckM2
+## CheckM2 (~1 minute)
 
 # Activate Conda environment
 conda activate checkm2
@@ -357,6 +401,7 @@ cp 7_gunc/*maxCSS_level.tsv 7_gunc.tsv
 # Plotting the data
 # Create an output directory
 mkdir 7_gunc/plot
+
 # Loop through a list of files
 for file in 7_gunc/diamond_output/*.out; do\
    gunc plot \
@@ -396,29 +441,26 @@ rm -r 7_quast
 ############################################################
 ## Vertical sequencing coverage
 
+# Declare input directory
+reads_directory="3_fastplong"
 # Create output file
 echo -e Sample"\t"Coverage > 7_coverage.tsv
-
 # Loop through a list of files
 for file in 6_assemblies/*.fasta; do
     # Extract assembly name
     assembly=$(basename $file .fasta)
     # Extract assembly name
     sample=${assembly%%_*}
-
     # Inform the sample
     echo -e Calculating sequencing coverage for assembly: $assembly
     echo -e Assembly file: ${file}
-    echo -e R1 file: 3_fastp/${sample}_*_1.fq.gz
-    echo -e R2 file: 3_fastp/${sample}_*_2.fq.gz
-
+    echo -e Read file: "${reads_directory}"/${sample}_trimmed.fq.gz
     # Count bases in assembly
     bases_in_assembly=$(grep -v '^>' "$file" | tr -d '\n' | wc -c)
     echo -e Bases in assembly: $bases_in_assembly
-
     # Count bases in sequencing files
-    # bases_in_reads=$(zcat 3_fastp/"$sample"*.gz | awk 'NR%4==2 {print $0}' | tr -d '\n' | wc -c)
-    bases_in_reads=$(zcat 3_fastp/${sample}_*_1.fq.gz 3_fastp/${sample}_*_2.fq.gz | 
+    # bases_in_reads=$(zcat "${reads_directory}"/"$sample"*.gz | awk 'NR%4==2 {print $0}' | tr -d '\n' | wc -c)
+    bases_in_reads=$(zcat "${reads_directory}"/${sample}_trimmed.fq.gz | 
                      awk 'NR%4==2 {print length}' | paste -sd+ | bc)
     echo -e Bases in reads: $bases_in_reads
     # Calculate vertical coverage
@@ -438,7 +480,7 @@ done
 ############################################################
 
 ############################################################
-## GTDB-Tk (~1 minute)
+## GTDB-Tk
 
 # Requires 64GB of RAM if the species is not identified by the ANI screening step
 # Activate Conda environment
@@ -451,6 +493,9 @@ gtdbtk classify_wf \
 --mash_db /db/gtdbtk \
 --genome_dir 6_assemblies \
 --out_dir 8_gtdbtk
+
+# Deactivate Conda environment
+conda deactivate
 
 # Copy and rename output files
 if [ -f "8_gtdbtk/gtdbtk.bac120.summary.tsv" ]; then
@@ -466,14 +511,13 @@ zip -r 8_gtdbtk.zip 8_gtdbtk
 # Delete the output directory
 rm -r 8_gtdbtk
 
-# Deactivate Conda environment
-conda deactivate
 
 ############################################################
 ## TYGS
 
 # https://tygs.dsmz.de/user_requests/new
 # Send the files from 6_assemblies
+
 
 ############################################################
 ## JspeciesWS
@@ -490,8 +534,10 @@ conda deactivate
 
 # Create an output directory
 mkdir 9_mobsuite
+
 # Activate Conda environment
 conda activate mob_suite
+
 # Loop through a list of files
 for file in 6_assemblies/*.fasta; do
     # Extract sample name
@@ -501,6 +547,7 @@ for file in 6_assemblies/*.fasta; do
     --infile $file \
     --outdir 9_mobsuite/${sample}_mobsuite
 done
+
 # Deactivate Conda environment
 conda deactivate
 
@@ -509,7 +556,7 @@ conda deactivate
 if [ -f 9_mobsuite/contig_report_all.tsv ]; then
     rm 9_mobsuite/contig_report_all.tsv
 fi
-# Initialize control variable to check if the header was printed
+# Initialize control variable to check in the header was printed
 header_printed=0
 # Check if any contig_report.txt files exist
 if find 9_mobsuite/ -maxdepth 2 -type f -name "contig_report.txt" | grep -q .; then
@@ -626,7 +673,6 @@ for assembly in 10_assemblies_for_analysis/*.fsa; do
     awk '1' 9_mobsuite/"$sample"_mobsuite/contig_report.txt | while IFS=$'\t' read -r sample_id molecule_type primary_cluster_id secondary_cluster_id contig_id others; do
         # Ignore column names
         if [ "$sample_id" == "sample_id" ]; then
-            # Skip to the next iteration
             continue
         fi
         # Conditional: plasmid contig
@@ -656,6 +702,14 @@ zip -r 9_mobsuite.zip 9_mobsuite
 # Delete output file
 rm -r 9_mobsuite
 
+# Rename molecules from strain N116
+sed -i 's/plasmid-name=pAF922/chromosome=2/' 10_assemblies_for_analysis/N116.fsa
+sed -i 's/pAA693/pl_URK_4a/' 10_assemblies_for_analysis/N116.fsa
+sed -i 's/pAA694/pl_URK_4b/' 10_assemblies_for_analysis/N116.fsa
+sed -i 's/pAA696/pl_URK_4c/' 10_assemblies_for_analysis/N116.fsa
+
+
+
 # In case of a novel plasmid, you will have to change its temporary name given by MOB-suite to an appropriate and shorter name.
 
 ############################################################
@@ -683,3 +737,4 @@ rm -r 9_mobsuite
 # https://submit.ncbi.nlm.nih.gov/subs/sra/
 # Upload a table containing the sequencing metadata. A template (SRA: Metadata spreadsheet with sample names) is available in https://submit.ncbi.nlm.nih.gov/templates/.
 # Upload the .fq.gz files in directory 1_reads
+

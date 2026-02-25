753d4c92d4f27bfeedfc9de165564be3cee11927
Marcus V. C. Viana <131043224+canarioviana@users.noreply.github.com>
2026-02-25 21:15:55 +0100

    Update 2_assembly_short_reads.sh

--- 2_assembly_short_reads.sh
+++ 2_assembly_short_reads.sh
@@ -640,7 +640,7 @@ for assembly in 10_assemblies_for_analysis/*.fsa; do
             # Inform sample and contig
             echo Sample: "$sample" - Contig: "$new_contig_id"
             # Replace old header with the new one
-            sed -i "s/^>${contig_id}/>${new_contig_id}/" $assembly
+            sed -i "s/^>${contig_id}\$/>${new_contig_id}/" "$assembly"
         fi
         # Conditional: chromosome contig
         if [ "$molecule_type" == "chromosome" ]; then
@@ -649,7 +649,7 @@ for assembly in 10_assemblies_for_analysis/*.fsa; do
             # Inform sample and contig
             echo Sample: "$sample" - Contig: "$new_contig_id"
             # Replace old header with the new one
-            sed -i "s/^>${contig_id}/>${new_contig_id}/" $assembly
+            sed -i "s/^>${contig_id}\$/>${new_contig_id}/" "$assembly"
         fi
     done
 done
